52cbb38c92d5e4b75296ccc6bdc135d3af08b505
savmg <skbmasale941@gmail.com>
2024-02-05 09:08:49 +0530

    router-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import SignIn from "./pages/SignIn"
+import AppShell from "./pages/AppShell"
+
 function App() {
   return (
-    <div>
-      This is app
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppShell />}>
+          <Route index element={<SignIn />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
